57bff51fcba5a82038b85986cf59c034393e19ed
Dean <deikdesign@gmail.com>
2021-12-02 01:14:45 +0200

    Webpack

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import style from './main.css';
+import component from './component.js';
+// import logo from './logo.svg';
+
+console.log('Hola!');
+console.log('adios');
+
+document.body.append(component());
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development', //production
+    entry: {
+        main: path.resolve(__dirname, 'src/app.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: '[name][ext]',
+        clean: true,
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+    
+    },
+    //loaders
+    module:{
+        rules: [
+            //css
+            {test: /\.css$/, use: ['style-loader', 'css-loader']},
+            //images
+            {test: /\.(svg|ico|png|webp|jpg|gif|jpeg)$/, type:'asset/resource'},
+            //js for babel
+            {test: /\.js$/, 
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                },
+            },
+          },
+        ],
+    },
+
+    //plugins
+    plugins: [new HtmlWebpackPlugin({
+        title: 'Just a Demo',
+        filename: 'index.html',
+        template: path.resolve(__dirname, 'src/temp.html')
+}),
+],
+};
